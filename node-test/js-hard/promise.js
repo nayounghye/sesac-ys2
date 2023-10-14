5c9f78db5a2399c693c7c6f95952a03d55e39fb5
nayounghye <yhna.dev@gmail.com>
2023-10-14 15:40:05 +0900

    20231014 열두번째 수업 : promise

--- node-test/js-hard/promise.js
+++ node-test/js-hard/promise.js
@@ -35,23 +35,80 @@ promise1(false)
     console.log(err);
   });
 
-function add(n1, n2, cb) {
-  setTimeout(function () {
-    let result = n1 + n2;
-    cb(result);
-  }, 1000);
+//--------------------------- callback 지옥 예시 ---------------------------
+
+// function add(n1, n2, cb) {
+//   setTimeout(function () {
+//     let result = n1 + n2;
+//     cb(result);
+//   }, 1000);
+// }
+
+// function mul(n, cb) {
+//   setTimeout(function () {
+//     let result = n * 2;
+//     cb(result);
+//   }, 700);
+// }
+
+// function sub(n, cb) {
+//   setTimeout(function () {
+//     let result = n - 1;
+//     cb(result);
+//   }, 500);
+// }
+
+// add(4, 3, function (x) {
+//   console.log('1: ', x);
+//   mul(x, function (y) {
+//     console.log('2: ', y);
+//     sub(y, function (z) {
+//       console.log('3: ', z);
+//     });
+//   });
+// });
+
+//--------------------------- callback 지옥 예시를 promise로 바꿔보자! ---------------------------
+function add(n1, n2) {
+  return new Promise((resolve) => {
+    setTimeout(function () {
+      let result = n1 + n2;
+      resolve(result);
+    }, 1000);
+  });
 }
 
-function mul(n, cb) {
-  setTimeout(function () {
-    let result = n * 2;
-    cb(result);
-  }, 700);
+function mul(n) {
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      let result = n * 2;
+      //   resolve(result);
+      reject('오류 발생');
+    }, 700);
+  });
 }
 
-function sub(n, cb) {
-  setTimeout(function () {
-    let result = n - 1;
-    cb(result);
-  }, 500);
+function sub(n) {
+  return new Promise((resolve) => {
+    setTimeout(function () {
+      let result = n - 1;
+      resolve(result);
+    }, 500);
+  });
 }
+
+add(4, 3)
+  .then((result) => {
+    console.log('1: ', result);
+    return mul(result);
+  })
+  .then((result) => {
+    console.log('2: ', result);
+    return sub(result);
+  })
+  .then((result) => {
+    console.log('3: ', result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
