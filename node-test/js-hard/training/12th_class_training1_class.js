0415bf6dd162d0da7eb866d08188b28ca5c4d354
nayounghye <yhna.dev@gmail.com>
2023-10-15 01:18:11 +0900

    수업12_실습1-3_클래스 상속2

--- node-test/js-hard/training/12th_class_training1_class.js
+++ node-test/js-hard/training/12th_class_training1_class.js
@@ -14,6 +14,7 @@ let rec1 = new Shape(3, 4);
 console.log(rec1.getArea());
 
 //-------------------------- 수업12_실습1-2. 클래스 상속1 --------------------------
+// 직사각형
 class Rectangle extends Shape {
   constructor(width, height) {
     super(width, height);
@@ -27,13 +28,14 @@ class Rectangle extends Shape {
   }
 }
 
-let rec2 = new Rectangle(3, 4);
+// let rec2 = new Rectangle(3, 4);
 rec2.getArea();
 rec2.getDiagonal();
-console.log(
-  `직사각형 넓이 : ${rec2.getArea()}, 대각선 길이: ${rec2.getDiagonal()}`
-);
+// console.log(
+//   `직사각형 넓이 : ${rec2.getArea()}, 대각선 길이: ${rec2.getDiagonal()}`
+// );
 
+// 삼각형
 class Triangle {
   constructor(width, height) {
     this.width = width;
@@ -47,16 +49,27 @@ class Triangle {
 let rec3 = new Triangle(3, 4);
 console.log(`삼각형 넓이 : ${rec3.getArea()}`);
 
-// 원 너비 구하기 부터 하면 된다!
-class Circle {
-  constructor(width, height) {
-    this.width = width;
-    this.height = height;
+// 원
+class Circle extends Shape {
+  constructor(width, height, radius) {
+    super(width, height);
+    this.radius = radius;
   }
   //메소드
   getArea() {
-    return this.width * this.height;
+    return this.radius ** 2 * 3.14;
   }
 }
 
-//-------------------------- 수업12_실습1-3. 클래스 상속 --------------------------
+let rec4 = new Circle(3, 4, 5);
+rec4.getArea();
+console.log(`원 넓이 : ${rec4.getArea()}`);
+
+//-------------------------- 수업12_실습1-3. 클래스 상속2 --------------------------
+
+// let rec2 = new Rectangle(3, 4);
+// console.log(`직사각형 넓이 : ${rec2.getArea()}`);
+// let rec3 = new Triangle(3, 4);
+// console.log(`삼각형 넓이 : ${rec3.getArea()}`);
+// let rec4 = new Circle(3, 4, 5);
+// console.log(`원 넓이 : ${rec4.getArea()}`);
