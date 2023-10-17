1d376654c2203af192ac43ae08034513208c5bcf
nayounghye <yhna.dev@gmail.com>
2023-10-17 15:30:21 +0900

    수업13_실습1_get으로 정보받기

---
+++ node-backend/02-form-practice/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+// -------- index.js 코드에 기본적으로 있어야하는 코드들 --------
+
+// const express = require('express');
+// const app = express();
+// const PORT = 8000;
+
+// app.set('view engine', 'ejs');
+
+// // localhost:8000 url 접속에 대한 응답을 위해 만든 코드
+// app.get('/', function (req, res) {
+//   res.render('index');
+// });
+
+// app.listen(PORT, function () {
+//   console.log(`Sever Open: ${PORT}`);
+// });
+
+// -------- index.js 코드에 기본적으로 있어야하는 코드들 끝 --------
