d84b3e3ba4a6cbca6d43e0a899b8cbcb0da3794a
nayounghye <yhna.dev@gmail.com>
2023-11-28 17:04:07 +0900

    수업31_실습1_jsx

--- react-proj/01-component-jsx/src/App.js
+++ react-proj/01-component-jsx/src/App.js
@@ -2,12 +2,37 @@ import "./App.css";
 import ClassComponent from "./components/ClassComponent";
 import FuncComponent from "./components/FuncComponent";
 
+const name = "테리";
+const animal = "요크셔테리어";
+const style = { textDecoration: "underline" };
+const a = 930;
+const b = 214;
 function App() {
   return (
     <div className="App">
-      <FuncComponent />
+      {/* <ClassComponent />
+      <FuncComponent /> */}
       {/* <FuncComponent></FuncComponent> */}
-      <ClassComponent />
+      <h1>Jsx 실습1</h1>
+      <h2>
+        <a style={style}>{name}</a>는 <a style={style}>{animal}</a>입니다.
+        <br />제 반려 동물의 이름은&nbsp;
+        <a style={style}>{name}</a>입니다.
+      </h2>
+      <h1>Jsx 실습2</h1>
+      <h2>{3 + 5 == 8 ? "정답입니다!" : "오답입니다!"}</h2>
+      <h1>Jsx 실습3</h1>
+      <h2>{a > b && "a가 b보다 큽니다."}</h2>
+      <h1>Jsx 실습4</h1>
+      <div className="prac-css1">
+        <h2 className="prac-css2">로그인</h2>
+        <div className="prac-css3">
+          아이디 : <input type="text"></input>
+          <br />
+          <br />
+          비밀번호 : <input type="password"></input>
+        </div>
+      </div>
     </div>
   );
 }
--- react-proj/01-component-jsx/src/components/FuncComponent.js
+++ react-proj/01-component-jsx/src/components/FuncComponent.js
@@ -1,6 +1,6 @@
 // 함수형 컴포넌트 작성법 1
 // export default FuncComponent = () => {
-//   return;
+//   return
 //   <div>Function Component 입니다!</div>;
 // };
 
@@ -11,6 +11,7 @@
 //);
 // }
 // export default FuncComponent;
+import image from "./Jordy.jpeg";
 
 function FuncComponent() {
   const text = "jordy";
@@ -24,6 +25,7 @@ function FuncComponent() {
       return "누구세요?";
     }
   };
+  const style = { fontSize: "20px", color: "red" };
 
   return (
     <>
@@ -36,8 +38,38 @@ function FuncComponent() {
 
       {/* 2. JS 문법 사용이 가능하다.(삼항 연산자를 조건에 따른 렌더링(간단한 것만 가능)) */}
       <h4>{name == "naraka" ? "안녕하세요!" : "누구세요?"}</h4>
-      {/* 2-1. 복잡한 조건을 이용하고 싶다면 위에서 함수로 만들고 밑에서 불러오면 된다. */}
+
+      {/* 2-1. 복잡한 조건을 이용하고 싶다면 위에서 함수(18~26번째줄)로 만들고 밑에서 불러오면 된다. */}
       <h4>{ifRenderTest()} </h4>
+
+      {/* 2-2. 조건에 따른 렌더링 (논리연산자 : &&) */}
+      {/* && : 앞의 내용이 참이면 뒷 내용을 아예 읽지 않음. 바로 참인 곳에서 종료됨. 거짓일 때 리턴값을 설정할 필요가 없음!*/}
+      <h5>{name === "naraka" && "나라카다!"}</h5>
+
+      {/* 3. inline style 적용 방법 -> style 속성 값으로 객체{} 전달 */}
+      <div style={{ fontSize: "30px", color: "red" }}>안녕하니니!</div>
+      {/* 3-1. inline style 적용 방법 -> 위에서 선언하고 밑에서 불러오면 된다. */}
+      <div style={style}>반갑니니!</div>
+
+      {/* 4. class와 onclick을 jsx에서 사용하기 */}
+      {/* <div class="" onclick="함수();" > -> 원래 html에서 사용하는 방식 */}
+      <div className="test-css">jsx에서 css사용하기(className)</div>
+
+      {/* html에서는 onclick안에 함수를 '호출'해서 사용함, jsx에서는 함수를 '선언'해서 사용한다. */}
+      <button
+        onClick={() => {
+          console.log("hello");
+        }}
+      >
+        버튼
+      </button>
+
+      {/* 5. 종료 태그는 필수! */}
+      <br />
+      {/* "/" 경로는 public 폴더 안을 의미한다. */}
+      <img src="/logo192.png" />
+      {/* src 폴더 내부 이미지를 사용할 경우 -> 위에서 이미지를 import 해서 가져오기! */}
+      <img src={image} style={{ width: "100px", height: "120px" }} />
     </>
   );
 }
