d657825ced4e881374208056e6d403f17941203e
nayounghye <yhna.dev@gmail.com>
2023-12-13 00:20:32 +0900

    수업37_실습1_redux

--- react-proj/08-redux/src/AppPrac1.js
+++ react-proj/08-redux/src/AppPrac1.js
@@ -1,22 +1,36 @@
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
-import { increase } from "./store/counterReducer";
-import { decrease } from "./store/counterReducer";
+import { plus, minus } from "./store/PracReducer";
+import { useState } from "react";
 
 function BankBox() {
-  const number = useSelector((state) => state.counter.number);
+  const money = useSelector((state) => state.counter.money);
   const isData = useSelector((state) => state.isData);
   const dispatch = useDispatch();
+  const [amount, setAmount] = useState(0); // 사용자가 입금할 금액
+
+  // 사용자가 입력한 값을 상태에 반영
+  const handleInputChange = (e) => {
+    setAmount(e.target.value);
+  };
+
+  // 입금
+  const handlePlus = () => {
+    dispatch(plus(Number(amount)));
+  };
+
+  // 출금
+  const handleMinus = () => {
+    dispatch(minus(Number(amount)));
+  };
+
   return (
-    <div className="">
+    <div className="bankbox">
       <h1>NINIZ 은행</h1>
-      <h2>잔액 : {number}</h2>
-      <input type="number" onChange={() => {}}></input>
-      <button onClick={() => dispatch(increase())}>입금</button>
-      <button onClick={() => dispatch(decrease())}>출금</button>
-      <button onClick={() => dispatch({ type: "CHANGE", payload: { number } })}>
-        CHANGE
-      </button>
+      <h2>잔액 : {money}</h2>
+      <input type="number" onChange={handleInputChange} value={amount}></input>
+      <button onClick={handlePlus}>입금</button>
+      <button onClick={handleMinus}>출금</button>
     </div>
   );
 }
---
+++ react-proj/08-redux/src/store/PracReducer.js
@@ -0,0 +1,27 @@
+// counter를 붙인 이유는 다른 reducer와 헷갈리지 말라고 표시한것!
+const PLUS = "counter/PLUS";
+const MINUS = "counter/MINUS";
+
+// 발생할 수 있는 action을 return 하는 함수이다.
+// 필요성 : action type의 이름이 바뀐다고 가정하면 action 을 발생시키는 모든 곳(dispatch를 사용한 모든 곳)에서
+// action.type을 하나하나 찾아헤매면서 다 변경해줘야 하므로, 이런 번거로움을 해결하기 위해 사용한다.
+export const plus = (amount) => ({ type: PLUS, payload: amount });
+export const minus = (amount) => ({ type: MINUS, payload: amount });
+
+const initialValue = { money: 0 };
+// state : {number : 100}
+const counterReducer = (state = initialValue, action) => {
+  switch (action.type) {
+    // 액션이 "INCREMENT"이거나, INCREMENT를 만났을 때 리턴값을 보낸다.
+    case "PLUS": // INCREMENT 상수 선언 전 버전 3
+    case PLUS: // INCREMENT 상수 선언 후 버전 3-1
+      return { ...state, money: state.money + Number(action.payload) };
+    case "MINUS": // DECREMENT 상수 선언 전 버전 3
+    case MINUS: // DECREMENT 상수 선언 후 버전 3-1
+      return { ...state, money: state.money - Number(action.payload) };
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
