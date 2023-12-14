62f94808977380d9ac13620960b6a8223b8de8d1
nayounghye <yhna.dev@gmail.com>
2023-12-14 18:23:58 +0900

    수업38_실습4_react with ts

--- react-proj/09-react-with-ts/src/index.tsx
+++ react-proj/09-react-with-ts/src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
+  // as: type 단언 
+  // ts컴파일러가 null, undefined가 가능하다고 해석하고 있고, 
+  // 우리는 #root가 존재한다고 단언(확신)할 수 있는 상황이기 때문에, as를 사용해 type을 지정해줌.
+  //  document.getElementById("root") type이 THTMLElement이다 라는 뜻.
   document.getElementById("root") as HTMLElement
 );
 root.render(
