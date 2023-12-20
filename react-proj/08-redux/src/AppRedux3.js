d2a0f3429f9000a14d8bef98f802f7088f39c680
nayounghye <yhna.dev@gmail.com>
2023-12-20 23:57:41 +0900

    231220 redux 수정

--- react-proj/08-redux/src/AppRedux3.js
+++ react-proj/08-redux/src/AppRedux3.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { Box1Container } from "./containers/BoxesContainers";
 
 // containers 폴더 : redux store에 직접적으로 접근하는 컴포넌트를 모아두기 위해서 생성
-// components 폴더 :  redux store에 직접적으로 접근하지 않고,보통 presentational 컴포넌트만 저장.
+// components 폴더 :  redux store에 직접적으로 접근하지 않고,보통 presentational 컴포넌트만 저장한다.
 
 function AppRedux3() {
   return (
